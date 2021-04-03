31233bc24e63a01b417f645aaabeaf8a78f54139
Shivam-Shandilya1 <shivamshandilya12@gmail.com>
2021-04-03 17:15:57 +0530

    remove red color

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var count = 0
+var count = 0;
+var numclick = 0;
 $(".blue").on("click", function() {
     $(this).removeClass("blue");
     $(this).addClass("red");
+    numclick+=1;
 });
 $(".blue").on("click",function()
     {
         count+=1;
+        if(count>2)
+        {
+            $(".red").removeClass("red").addClass("blue");
+        }
     });
-    if(count>2)
-    {
-        
-    }
+   
