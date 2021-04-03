6f3a609e4d5d1a7838d32daeaf8d675df85feda4
Shivam-Shandilya1 <shivamshandilya12@gmail.com>
2021-04-03 17:48:23 +0530

    Final Commit

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-var count = 0,secondLast = count-2;
+var count = 0;
 var numclick = 0;
 $(".blue").on("click", function() {
     
    
     numclick+=1;
     count+=1;
+    if(count>2)
+    {
+        var  last =count -2;
+    };
         $(this).removeClass("blue");
         console.log("w"+count);
+        console.log("w"+last);
         $(this).addClass("red w"+count);
         if(count>2)
         {
             
-            $(".w"+count-2).removeClass("red").removeClass("w1").addClass("blue");
+            $(".w"+last).removeClass("red").removeClass("w"+last).addClass("blue");
         }
     
 });
